1701f139fa0f64246dcd3a3105da4fcbe8afdefb
unknown <andreisokolov190@gmail.com>
2022-09-27 15:16:18 +0300

    Обновление RP-001

--- Практическая/Program.cs
+++ Практическая/Program.cs
@@ -1,9 +1,10 @@
-﻿Console.Write("Введите номер операции: 1. Сложить 2 числа 2. Вычесть первое из второго 3. Перемножить два числа 4. Разделить первое на второе 5. Возвести в степень N первое число 6. Найти квадратный корень из числа 7. Найти 1 процент от числа 8. Найти факториал из числа 9. Выйти из программы ");
-int oper = Convert.ToInt32(Console.ReadLine());
-while (oper == 9)
+﻿int oper;
+do
 {
+    Console.Write("Введите номер операции: 1. Сложить 2 числа 2. Вычесть первое из второго 3. Перемножить два числа 4. Разделить первое на второе 5. Возвести в степень N первое число 6. Найти квадратный корень из числа 7. Найти 1 процент от числа 8. Найти факториал из числа 9. Выйти из программы ");
+    oper = Convert.ToInt32(Console.ReadLine());
     switch (oper)
-    {
+    {     
         case 1:
             Console.WriteLine("Напишите два числа");
             int nm1 = Convert.ToInt32(Console.ReadLine());
@@ -12,44 +13,45 @@ while (oper == 9)
             break;
         case 2:
             Console.WriteLine("Напишите два числа");
-            int nm1 = Convert.ToInt32(Console.ReadLine());
-            int nm2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(nm2 - nm1);
+            int nm3 = Convert.ToInt32(Console.ReadLine());
+            int nm4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(nm4 - nm3);
             break;
         case 3:
             Console.WriteLine("Напишите два числа");
-            int nm1 = Convert.ToInt32(Console.ReadLine());
-            int nm2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(nm1 * nm2);
+            int nm5 = Convert.ToInt32(Console.ReadLine());
+            int nm6 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(nm5 * nm6);
             break;
         case 4:
             Console.WriteLine("Напишите два числа");
-            int nm1 = Convert.ToInt32(Console.ReadLine());
-            int nm2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(nm1 / nm2);
+            int nm7 = Convert.ToInt32(Console.ReadLine());
+            int nm8 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(nm7 / nm8);
             break;
         case 5:
             Console.WriteLine("Напишите два числа");
-            int nm1 = Convert.ToInt32(Console.ReadLine());
-            int nm2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"{nm1}^{nm2} = {(long)Math.Pow(nm1, nm2):N0} (0x{(long)Math.Pow(nm1, nm2):X})");
+            int nm9 = Convert.ToInt32(Console.ReadLine());
+            int nm10 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{nm9}^{nm10} = {(long)Math.Pow(nm9, nm10):N0} (0x{(long)Math.Pow(nm9, nm10):X})");
             break;
         case 6:
             Console.WriteLine("Напишите число");
-            Console.ReadLine()
-            int nm1 = Convert.ToInt32(Math.Sqrt(5.25));
-            Console.WriteLine(nm1);
+            int nm11 = Convert.ToInt32(Math.Sqrt(5.25));
+            Console.WriteLine(nm11);
             break;
         case 7:
-            int nm1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(nm1 % 1);
+            Console.WriteLine("Напишите число");
+            int nm12 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(nm12 % 1);
             break;
         case 8:
-            int nm1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(nm1 * nm2);
+            Console.WriteLine("Напишите число");
+            int nm13 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Факториал числа неизвестен");
             break;
         case 9:
             Console.WriteLine("Завершение программы.");
             break;
     }
-}
+} while (oper != 9);
